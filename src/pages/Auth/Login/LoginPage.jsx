c3e4379ce477199ffb14366f99814e54653f23c1
anthony.lifetech <anthony@lifetech.co.id>
2021-12-28 17:23:55 +0700

    Initial Formik

--- src/pages/Auth/Login/LoginPage.jsx
+++ src/pages/Auth/Login/LoginPage.jsx
@@ -3,34 +3,110 @@ import {
   Form,
   Button
 } from 'react-bootstrap'
+import styles from './LoginPage.module.css'
+import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
+import * as Yup from "yup";
+
 
 export default function LoginPage() {
+
+  const initialValuesLogin = {
+    email: "",
+    password: ""
+  }
+
+  const validationSchemaLogin = Yup.object({
+    email: Yup.string()
+      .email('Invalid Format Email')
+      .required('Please Input Email'),
+    password: Yup.string()
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!#%*?&]{8,20}$/,
+        'Password at Least Must Have One Uppercase, One Numeric and 8 Characters Long'
+      )
+      .required('Please Input Password')
+  })
+
+  const formikLogin = useFormik({
+    enableReinitialize: true,
+    initialValues: initialValuesLogin,
+    validationSchema: validationSchemaLogin,
+    onSubmit: (values, {resetForm}) => {
+      console.log("values", values)
+    }
+  })
+
+  const validationLogin = (fieldname) => {
+    if (formikLogin.touched[fieldname] && formikLogin.errors[fieldname]) {
+      return "is-invalid";
+    }
+    if (formikLogin.touched[fieldname] && !formikLogin.errors[fieldname]) {
+      return "is-valid";
+    }
+    return "";
+  };
+
   return (
-    <div className="container">
-    <div className="row">
-      <div className="col-md-12">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+    <div className={styles.container}>
+      <div className="d-flex flex-column align-items-center">
+        <div className='auth-title'>Login Account</div>
+        <div className='auth-desc text-muted'>Enter Your Email and Password</div>
+      </div>
+      <div className={styles.wrapperForm}>
+        <Form onSubmit={formikLogin.handleSubmit}>
+          <Form.Group className="mb-2" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+            <Form.Control 
+              type="email" 
+              className={validationLogin("email")}
+              placeholder="Enter email" 
+              name="email"
+              value={formikLogin.values.email}
+              onChange={formikLogin.handleChange}
+            />
+            {formikLogin.touched.email &&
+            formikLogin.errors.email ? (
+              <div className="text-danger">
+                {formikLogin.errors.email}
+              </div>
+            ) : null}
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group className="mb-2" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control 
+              type="password" 
+              className={validationLogin("password")}
+              name="password"
+              placeholder="Password" 
+              value={formikLogin.values.password}
+              onChange={formikLogin.handleChange}
+            />
+            {formikLogin.touched.password &&
+            formikLogin.errors.password ? (
+              <div className="text-danger">
+                {formikLogin.errors.password}
+              </div>
+            ) : null}
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+
+          {/* <Form.Group className="mb-2" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
+          </Form.Group> */}
+
+          <div className="d-flex justify-content-between mb-2">
+            <div className={`${styles.button } text-muted`}>Forgot Password?</div>
+            <Link to="/auth/register">
+              <div className={`${styles.button} text-primary`}>Don't Have Account? Signup</div>
+            </Link>
+          </div>
+
           <Button variant="primary" type="submit">
             Submit
           </Button>
         </Form>
       </div>
-    </div>
     </div>
   )
 }
--- src/pages/Auth/Register/RegisterPage.jsx
+++ src/pages/Auth/Register/RegisterPage.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import {
+  Form,
+  Button
+} from 'react-bootstrap'
+import styles from './RegisterPage.module.css'
+import { Link } from 'react-router-dom'
+
 
 export default function RegisterPage() {
   return (
-    <div>
-      This is Register Page!
+    <div className={styles.container}>
+      <div className="d-flex flex-column align-items-center">
+        <div className='auth-title'>Login Account</div>
+        <div className='auth-desc text-muted'>Register and get free trial, no pre payment and credit card needed</div>
+      </div>
+      <div className={styles.wrapperForm}>
+        <Form>
+          <Form.Group className="mb-2" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formBasicPhoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="number" placeholder="Phone Number" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group className="mb-2" controlId="formBasicConfirmPassword">
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control type="password" placeholder="Confirm Password" />
+          </Form.Group>
+
+          {/* <Form.Group className="mb-2" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group> */}
+
+          <div className="d-flex justify-content-between mb-2">
+            <div className={`${styles.button } text-muted`}>Forgot Password?</div>
+            <Link to="/auth/login">
+              <div className={`${styles.button} text-primary`}>Already Have Account? Login</div>
+            </Link>
+          </div>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   )
 }
