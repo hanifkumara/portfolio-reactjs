d659031aaa78314660c20c35a62c850f1272541f
unknown <hnflasting@gmail.com>
2022-01-08 11:00:05 +0700

    New Changes

--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import styles from './index.module.css'
 import { Header } from '../../components'
+import SearchGoogleImage from '../../assets/images/screencapture-google-search-2022-01-03-22_26_10.png'
 
 export default function index() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapperContent}>
+        <div className={styles.card}>
+          <div className={styles.browser}>
+            <div className={styles.circle}></div>
+            <div className={styles.circle}></div>
+            <div className={styles.circle}></div>
+          </div>
+          <img src={SearchGoogleImage} alt="" className={styles.image} />
+        </div>
+      </div>
     </div>
   )
 }
